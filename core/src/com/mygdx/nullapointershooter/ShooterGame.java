800b03735f663ddaaf7cc318946a5f32a1813da5
Ankit Shah <akishah92@gmail.com>
2017-11-12 14:06:56 -0500

    A Hero Rises Up : Adding game background and spaceship. Adding code to animate the spaceship.

--- core/src/com/mygdx/nullapointershooter/ShooterGame.java
+++ core/src/com/mygdx/nullapointershooter/ShooterGame.java
@@ -3,31 +3,44 @@ package com.mygdx.nullapointershooter;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ShooterGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture background;
+	private AnimatedSprite spaceshipAnimated;
 	
 	@Override
 	public void create () {
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		background = new Texture("space-background.png");
+		Texture spaceshipTexture = new Texture("spaceship-spritesheet.png");
+		Sprite spaceshipSprite = new Sprite(spaceshipTexture);
+		spaceshipAnimated = new AnimatedSprite(spaceshipSprite);
+		spaceshipAnimated.setPosition(background.getWidth()/2, 0);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
+		spaceshipAnimated.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
