bdeafeeaec954800028898c834b59f04cdfd6a78
Ankit Shah <akishah92@gmail.com>
2017-11-12 16:02:37 -0500

    Thrusters Activated : Adding code to move the ship left and right within the screen limits.

--- core/src/com/mygdx/nullapointershooter/AnimatedSprite.java
+++ core/src/com/mygdx/nullapointershooter/AnimatedSprite.java
@@ -6,9 +6,13 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
+import sun.net.InetAddressCachePolicy;
+
 public class AnimatedSprite {
+    private static final int SHIP_SPEED = 300;
     private static final int FRAMES_ROW = 2;
     private static final int FRAMES_COL = 2;
 
@@ -16,6 +20,7 @@ public class AnimatedSprite {
     private Animation animation;
     private TextureRegion[] frames;
     private TextureRegion currentFrame;
+    private Vector2 velocity = new Vector2();
 
     private float stateTime;
 
@@ -23,7 +28,7 @@ public class AnimatedSprite {
 
         this.sprite = sprite;
         Texture texture = sprite.getTexture();
-        TextureRegion[][] temp = TextureRegion.split(texture, texture.getWidth()/FRAMES_COL, texture.getHeight()/FRAMES_ROW);
+        TextureRegion[][] temp = TextureRegion.split(texture, (int) getSpriteWidth(), texture.getHeight()/FRAMES_ROW);
         frames = new TextureRegion[FRAMES_COL * FRAMES_ROW];
         int index = 0;
         for (int i = 0; i < FRAMES_ROW; i++) {
@@ -44,7 +49,41 @@ public class AnimatedSprite {
     }
 
     public void setPosition(float x, float y) {
-        float widthOffset = sprite.getWidth()/FRAMES_COL;
-        sprite.setPosition(x - widthOffset/2, y);
+        sprite.setPosition(x - getSpriteCenterOffset(), y);
+    }
+
+    public float getSpriteCenterOffset() {
+        return getSpriteWidth() / 2;
+    }
+
+    private float getSpriteWidth() {
+        return sprite.getWidth() / FRAMES_COL;
+    }
+
+    public void moveRight() {
+        velocity = new Vector2(SHIP_SPEED, 0);
+    }
+
+    public void moveLeft() {
+        velocity = new Vector2(-SHIP_SPEED, 0);
+    }
+
+    public int getX() {
+        return (int) (sprite.getWidth() + getSpriteCenterOffset());
+    }
+
+    public void move() {
+        int xMovement = (int) (velocity.x * Gdx.graphics.getDeltaTime());
+        sprite.setPosition(sprite.getX() + xMovement, 0);
+
+        if (sprite.getX() < 0) {
+            sprite.setX(0);
+            velocity.x = 0;
+        }
+
+        if (sprite.getX() + getSpriteWidth() > ShooterGame.SCREEN_WIDTH) {
+            sprite.setX(ShooterGame.SCREEN_WIDTH - getSpriteWidth());
+            velocity.x = 0;
+        }
     }
 }
--- core/src/com/mygdx/nullapointershooter/ShooterGame.java
+++ core/src/com/mygdx/nullapointershooter/ShooterGame.java
@@ -9,6 +9,8 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ShooterGame extends ApplicationAdapter {
+	public static final int SCREEN_WIDTH = 800;
+	public static final int SCREEN_HEIGHT = 480;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture background;
@@ -18,13 +20,13 @@ public class ShooterGame extends ApplicationAdapter {
 	public void create () {
 
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, SCREEN_WIDTH, SCREEN_HEIGHT);
 		batch = new SpriteBatch();
 		background = new Texture("space-background.png");
 		Texture spaceshipTexture = new Texture("spaceship-spritesheet.png");
 		Sprite spaceshipSprite = new Sprite(spaceshipTexture);
 		spaceshipAnimated = new AnimatedSprite(spaceshipSprite);
-		spaceshipAnimated.setPosition(background.getWidth()/2, 0);
+		spaceshipAnimated.setPosition(SCREEN_WIDTH/2, 0);
 	}
 
 	@Override
@@ -37,8 +39,24 @@ public class ShooterGame extends ApplicationAdapter {
 		batch.draw(background, 0, 0);
 		spaceshipAnimated.draw(batch);
 		batch.end();
+
+		handleInput();
+		spaceshipAnimated.move();
 	}
-	
+
+	private void handleInput() {
+		if (Gdx.input.isTouched()) {
+			int xTouch = Gdx.input.getX();
+
+			if (xTouch > spaceshipAnimated.getX()) {
+				spaceshipAnimated.moveRight();
+			}
+			else {
+				spaceshipAnimated.moveLeft();
+			}
+		}
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
